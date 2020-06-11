d10c1769404da0ea92e562350d70becbd3a09377
ForrestGG <903524121@qq.com>
2020-06-11 16:52:21 +0800

    添加项目文件。

---
+++ CLSample/RevitCmd.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLSample
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    internal class RevitCmd : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            MainWindow main = new MainWindow();
+            main.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
